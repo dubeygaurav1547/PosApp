01212a14ab7c347d440a8aeb3782019eb9aaaf03
Gaurav Dubey <dubeygaurav778@gmail.com>
2024-08-26 17:21:01 +0530

    Framework init

--- src/main/java/com/shield3/framework/data/Data.java
+++
@@ -1,8 +0,0 @@
-package com.shield3.framework.data;
-
-public class Data {
-    public static String stagingUrl = "https://staging.app-preview.shield3.com";
-    public static String email = "shield3demo+staging@gmail.com";
-    public static String address = "0xd8da6bf26964af9d7eed9e03e53415d37aa96041";
-    public static String addressName = "automation Address";
-}
